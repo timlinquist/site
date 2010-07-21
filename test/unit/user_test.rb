e766a69b6618575c42b38ec8604aea77466fa994
CobyR <randquistcp@gmail.com>
2010-07-20 17:29:37 -0700

    added user_tests and removed development log

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :users
+
+  model_template_for User do
+    {
+      :username  => "tester",
+      :email     => "test@test.com",
+      :password  => '123456',
+      :time_zone => 'Pacific Time (US & Canada)'
+    }
   end
+
+  test_validations_for :username, :uniqueness
+  test_validations_for :email, :uniqueness
+  test_validations_for :username, :presence
+  test_validations_for :time_zone, :presence
+
+  def test_create
+    u = User.new
+    u.username = "GlourisTester"
+    u.email = "test@test.com"
+    u.password = "123456"
+    u.time_zone = "Pacific Time (US & Canada)"
+
+    assert u.save
+    assert_not_equal nil, u.password_hash
+    assert_not_equal nil, u.password_salt
+  end
+
 end
