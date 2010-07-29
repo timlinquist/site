2ae1983cb6bed65778683cb18819922d3a398787
CobyR <randquistcp@gmail.com>
2010-07-28 22:08:53 -0700

    changes to user tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -5,10 +5,11 @@ class UserTest < ActiveSupport::TestCase
 
   model_template_for User do
     {
-      :username  => "tester",
-      :email     => "test@test.com",
-      :password  => '123456',
-      :time_zone => 'Pacific Time (US & Canada)'
+      :username              => "tester",
+      :email                 => "test@test.com",
+      :password              => '123456',
+      :password_confirmation => '123456',
+      :time_zone             => 'Pacific Time (US & Canada)'
     }
   end
 
@@ -17,16 +18,61 @@ class UserTest < ActiveSupport::TestCase
   test_validations_for :username, :presence
   test_validations_for :time_zone, :presence
 
-  def test_create
-    u = User.new
-    u.username = "GlourisTester"
-    u.email = "test@test.com"
-    u.password = "123456"
-    u.time_zone = "Pacific Time (US & Canada)"
+  test "create user should set password_salt and password_hash" do
+    u = create_user
 
     assert u.save
-    assert_not_equal nil, u.password_hash
-    assert_not_equal nil, u.password_salt
+    refute_equal nil, u.password_salt, "password_salt was nil"
+    refute_equal nil, u.password_hash, "password_hash was nil"
   end
 
+  test "attributes are setable" do
+    u = create_user
+
+    assert_equal 'tester', u.username
+    assert_equal 'test@test.com', u.email
+    assert_equal 'Pacific Time (US & Canada)', u.time_zone
+
+  end
+
+  test "user with mismatched passwords doesn't save" do
+    u = create_user
+
+    u.password_confirmation = '654321'
+
+    refute u.save
+  end
+
+  test "password hash says the same when no password is set" do
+    u1a = create_user
+
+    assert u1a.password?("123456")
+
+    u1b = User.find(u1a.id)
+    u1b.first_name = "Test"
+    u1b.last_name = "Master"
+
+    assert u1b.save!
+
+    assert u1b.password?("123456"), "password was not messed up"
+    assert_equal u1a.password_salt, u1b.password_salt
+    assert_equal u1a.password_hash, u1b.password_hash
+  end
+
+  test "password hash should change when password is set, salt does not" do
+    u = create_user
+
+    original_salt = u.password_salt
+    original_hash = u.password_hash
+
+    u.password = "newpassword"
+    u.password_confirmation = "newpassword"
+
+    assert u.save!
+
+    assert_equal original_salt, u.password_salt,
+      "password_salt changed..."
+    refute_equal original_hash, u.password_hash,
+      "password_hash remained the same"
+  end
 end
