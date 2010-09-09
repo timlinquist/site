43ff3865a0d7defbf3c28730b61d4fbe8fe4b30d
CobyR <randquistcp@gmail.com>
2010-09-09 11:10:35 -0600

    added option for ?go=true on events/short_code url to bypass redirection if the conference isn't ready and is redirecting

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,13 +5,12 @@ class EventsController < ApplicationController
   def index
     @events = Event.find(:all, :order => 'start_at desc')
     render :layout => 'admin'
-
   end
 
   def show
     @event = Event.find_by_identifier(params[:id])
 
-    unless @event.ready
+    unless @event.ready  && params[:go]
       redirect_to "http://#{@event.short_code}.confreaks.com", :status => 302
     end
   end
