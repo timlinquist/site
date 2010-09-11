37da8e5ccb8528c05e467bb617ddb4be141b161b
CobyR <randquistcp@gmail.com>
2010-09-11 00:17:07 -0600

    changed asset display

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -45,15 +45,36 @@ class Asset < ActiveRecord::Base
     @duration
   end
 
+  def size
+    if width > 0 and height > 0
+      "#{width}x#{height}"
+    else
+      ""
+    end
+  end
+
   def display_description
-    if description.blank?
-      if asset_type.nil?
+    hlp = Object.new.extend(ActionView::Helpers::NumberHelper)
+    elements = Array.new
+    if asset_type.description == "Video" then
+      elements << (description.blank? ? nil : description)
+      elements << size
+      elements << data_content_type
+      elements << hlp.number_to_human_size(data_file_size)
+      elements << duration
+      elements.compact.join(" - ")
+    elsif asset_type.description == "Audio" then
+      elements << description.blank? ? nil : description
+      elements << data_content_type
+      elements << hlp.number_to_human_size(data_file_size)
+      elements << duration
+      elements.compact.join(" - ")
+    else
+      if description.blank?
         "no description available"
       else
-        asset_type.description
+        description
       end
-    else
-      description
     end
   end
 
@@ -61,28 +82,33 @@ class Asset < ActiveRecord::Base
     if data
       file = data.path
 
-      tmp1 = RVideo::Inspector.new :file => file
+      begin
+        tmp1 = RVideo::Inspector.new :file => file
 
-      @raw_response = tmp1.raw_response
+        @raw_response = tmp1.raw_response
 
-      pos1 = (tmp1.raw_response =~ /Duration:/)
+        pos1 = (tmp1.raw_response =~ /Duration:/)
 
-      unless pos1.nil?
-        @duration = tmp1.raw_response[(pos1 + 10),8]
-      else
-        @duration =""
-      end
+        unless pos1.nil?
+          @duration = tmp1.raw_response[(pos1 + 10),8]
+        else
+          @duration =""
+        end
 
-      pos2 = (tmp1.raw_response =~ /Video:/)
+        pos2 = (tmp1.raw_response =~ /Video:/)
 
-      unless pos2.nil?
-        tmp2 = tmp1.raw_response[pos2,50]
-        @width, @height = tmp2[(tmp2 =~ /x/) - 4, 9].split(',')[0].split('x')
-      else
+        unless pos2.nil?
+          tmp2 = tmp1.raw_response[pos2,50]
+          @width, @height = tmp2[(tmp2 =~ /x/) - 4, 9].split(',')[0].split('x')
+        else
+          @width = 0
+          @height = 0
+        end
+      rescue
         @width = 0
         @height = 0
+        @duration = 0
       end
-
       true
     else
       false
