e384c8fee96a01254b073c538b7eb30b59dcf059
CobyR <randquistcp@gmail.com>
2010-09-17 01:10:55 -0700

    adjusted calendar display style.

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -11,14 +11,23 @@ module CalendarHelper
       :event_strips => @event_strips,
       :month_name_text => I18n.localize(@shown_month, :format => "%B %Y"),
       :previous_month_text => "<< " + month_link(@shown_month.last_month),
-      :next_month_text => month_link(@shown_month.next_month) + " >>"    }
+      :next_month_text => month_link(@shown_month.next_month) + " >>",
+      :day_names_height => 24,
+      :day_nums_height => 18,
+      :event_height => 18,
+      :event_margin => 1,
+      :event_padding_top => 1    
+    }
+
   end
 
   def event_calendar
     # args is an argument hash containing :event, :day, and :options
     calendar event_calendar_opts do |args|
       event = args[:event]
-      %(<a href="/events/#{event.id}" title="#{h(event.display_name)}">#{h(event.display_name)}</a>)
+      %(<a href="/events/#{event.short_code}" title="#{h(event.display_name)}">
+        #{h(event.display_name)}
+        </a>)
     end
   end
 end
