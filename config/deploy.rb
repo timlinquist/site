904f36658bf72a912c154c33911e6b6c20280eb8
CobyR <randquistcp@gmail.com>
2010-09-20 08:51:09 -0700

    user change based on DH failure

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ fail_message = "\nInvalid deploy_env specified\n\tUsage: cap {command} -S deploy
 
 begin; deploy_env; rescue NameError; set :deploy_env, 'integration' end
 
-set :user, 'cfprod'
+set :user, 'cfroot'
 set :domain, 'confreaks.net'
 set :use_sudo, false
 
