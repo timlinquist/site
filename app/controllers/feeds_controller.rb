7882bfbc9301cd21adb6c00bb360af8213227aa6
CobyR <randquistcp@gmail.com>
2010-10-13 11:11:24 -0700

    work on feeds, plus story http://www.pivotaltracker.com/story/show/5666234 about additional admin user info

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -6,4 +6,14 @@ class FeedsController < ApplicationController
     @title = "Confreaks, LLC - All Videos Feed"
     @updated = @videos.first.updated_at unless @videos.empty?
   end
+
+  def show
+    @event = Event.find_by_identifier(params[:id])
+    @videos = @event.videos(:order => 'post_date desc, recorded_at desc')
+
+    @title = @event.display_name
+    @updated = @videos.first.updated_at unless @videos.empty?
+
+    render 'index'
+  end
 end
