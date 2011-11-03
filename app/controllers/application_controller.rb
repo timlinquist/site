e99b5efda110a712a4f9517c15cd0249c89ab863
Steven R. Baker <steven@stevenrbaker.com>
2011-11-02 21:46:16 -0700

    this is a config option now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection
                        # for details
 
-  # Scrub sensitive parameters from your log
-  filter_parameter_logging :password
-
   before_filter :set_time_zone
 
   before_filter :log_history
