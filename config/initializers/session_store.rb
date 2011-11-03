58100138e99bf338ba15c41c539ccd8458abdcff
Steven R. Baker <steven@stevenrbaker.com>
2011-11-02 20:04:13 -0700

    more rails 3 generated stuff

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_v2a_session',
-  :secret      => '88fc9fbc952a95a076afdd77b0838b7fd21855c3e9ecff40437285bdceb06bd8ce27a5c4cf97558a67280ff0a1874a3cac8bfe0b62755dc25553d9101d99e84e'
-}
+Confreaks::Application.config.session_store :cookie_store, :key => '_confreaks_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# Confreaks::Application.config.session_store :active_record_store
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,5 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
 function remove_fields(link) {
   $(link).previous("input[type=hidden]").value = "1";
   $(link).up(".form-field-set").hide();
