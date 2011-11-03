a0334273ad2bdb0b944f7167d2a63877e5610b57
Steven R. Baker <steven@stevenrbaker.com>
2011-11-02 20:24:32 -0700

    there are no tests; no sense pretending for now

--- test/functional/admin/asset_types_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class Admin::AssetTypesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
--- test/functional/redirects_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RedirectsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_page_redirect
-  end
-end
