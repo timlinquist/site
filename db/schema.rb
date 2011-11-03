d117f99bd572882673e9dfdb15ed3013b78e67c9
Steven R. Baker <steven@stevenrbaker.com>
2011-11-02 21:02:51 -0700

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,11 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
@@ -125,7 +126,6 @@ ActiveRecord::Schema.define(:version => 20110725075252) do
     t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
   end
 
   create_table "rooms", :force => true do |t|
@@ -135,13 +135,6 @@ ActiveRecord::Schema.define(:version => 20110725075252) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  create_table "t", :id => false, :force => true do |t|
-    t.integer "i"
-    t.text    "t"
-  end
-
-  add_index "t", ["t"], :name => "t"
 
   create_table "twitter_accounts", :force => true do |t|
     t.integer  "user_id"
@@ -191,7 +184,7 @@ ActiveRecord::Schema.define(:version => 20110725075252) do
     t.datetime "image_updated_at"
     t.integer  "streaming_asset_id"
     t.string   "rating",             :default => "Everyone"
-    t.text     "abstract"
+    t.text     "abstract",           :default => ""
     t.datetime "post_date"
     t.boolean  "announce",           :default => false
     t.datetime "announce_date"
@@ -201,7 +194,6 @@ ActiveRecord::Schema.define(:version => 20110725075252) do
 
   add_index "videos", ["event_id"], :name => "by_event_id"
   add_index "videos", ["recorded_at"], :name => "by_recorded_at"
-  add_index "videos", ["title", "abstract"], :name => "title"
   add_index "videos", ["title"], :name => "by_title"
 
 end
