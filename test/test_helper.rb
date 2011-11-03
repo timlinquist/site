27320b0ca9168009a0241e9c0b23f0122b18e6c2
Steven R. Baker <steven@stevenrbaker.com>
2011-11-02 21:22:59 -0700

    test helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,25 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  def refute expr, message = nil
+    assert !expr, message
+  end
+
+  alias :refute_equal :assert_not_equal
+
+  def login user, password = "123456", &block
+    user = users(user) if Symbol === user
+    @request.session[:user_id] = user.id
+
+    if block_given?
+      yield user
+      @controller.session[:user_id] = nil
+    end
+
+    user
+  end
+
+  def logout
+    @request.session[:user_id] = nil
+  end
 end
