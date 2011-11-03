8b9824bd6a92e3c7403d4f81f3d440c6e0d9e59d
Steven R. Baker <steven@stevenrbaker.com>
2011-11-02 22:04:52 -0700

    use modelizer

---
+++ test/unit/event_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+require 'modelizer'
+
+class EventTest < ActiveSupport::TestCase
+  include Modelizer
+  fixtures :events, :conferences
+
+  model_template_for Event do
+    {
+      :conference_id => 1,
+      :name_suffix => 2010,
+      :short_code => 'test2010'
+    }
+  end
+
+  def test_new_event
+    e = create_event
+
+    assert e.save
+  end
+
+  def test_find_by_identifier
+    a = Event.find_by_identifier('mwrc2010')
+
+    assert_equal 1, a.id
+  end
+
+  def test_find_by_identifier_failure
+    a = Event.find_by_identifier('wrong')
+
+    refute a
+
+    b = Event.find_by_identifier(49494)
+
+    refute b
+  end
+end
---
+++ test/unit/video_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+require 'modelizer'
+
+class VideoTest < ActiveSupport::TestCase
+  include Modelizer
+  fixtures :videos, :presenters, :events
+
+  model_template_for Video do
+    {
+      :title => 'Writing Modular Ruby Code: Lessons Learned from Rails 3',
+      :event_id => events(:mwrc2010).id,
+      :recorded_at => Time.zone.now,
+      :available => true
+    }
+  end
+
+  test_validations_for :title, :presence
+  test_validations_for :recorded_at, :presence
+
+  test "random should return video with a streaming_video" do
+    v = Video.random
+
+    assert_not_nil v.streaming_video
+
+    v = videos(:no_streaming)
+
+    refute v.streaming_video
+  end
+
+  test "slug should return" do
+    v = videos(:valid)
+
+    assert v.slug
+  end
+end
