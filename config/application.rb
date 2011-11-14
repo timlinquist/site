86bf3f3d6634b8de8e6dac27dbd0ca402418b117
Steven R. Baker <steven@stevenrbaker.com>
2011-11-13 23:09:30 -0800

    port the rest of the configuration to rails3

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Confreaks
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Pacific Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -38,5 +38,12 @@ module Confreaks
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    config.after_initialize do
+      Disqus::defaults[:account] = "confreaks"
+      Disqus::defaults[:container_id] = "disqus_thread"
+      Disqus::defaults[:developer] = true
+      Disqus::defaults[:show_powered_by] = false
+    end
   end
 end
